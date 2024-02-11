1e96683fc14336d2f54b6cce5e2bf9052f91f953
abhi131297 <147903371+abhi131297@users.noreply.github.com>
2024-02-11 12:31:33 +0530

    Add files via upload

---
+++ CRUD_angular_project/src/test/java/com/Angularcrud/CrudAngularProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Angularcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAngularProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
